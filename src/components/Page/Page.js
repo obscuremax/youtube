31765da1c495f4b3142ae27352a581001a22a0ca
obscuremax <maxxam@yandex.ru>
2020-02-26 19:06:33 +0300

    qqq

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -11,18 +11,65 @@ export function Page(props) {
 
 	return boxData? (
 		<Wrap>
+			<div><Buttonleft>&lt;</Buttonleft></div>
+			<Container>
+				<ContainerPage>
 			{pages.map((p, index) => {
-				return <Span onClick={props.pageLeft} onMouseUp={props.getPageCurrent} onMouseDown={props.pageCount != 0 && ((props.pageCount - props.pageCurrent) <=1)? props.getBox():""} id={index}>{p}</Span>
+				return <div><Button onClick={props.pageLeft} onMouseUp={props.getPageCurrent} onMouseDown={props.pageCount != 0 && ((props.pageCount - props.pageCurrent) <=1)? props.getBox():""} id={index}>{p}</Button></div>
 			})}
+				</ContainerPage>
+			</Container>
+			<div><Buttonleft>&gt;</Buttonleft></div>
 		</Wrap>
 	): null
 }
 
 const Wrap = styled.div`
 text-align: center;
+display: flex;
+margin: 0 auto;
+/*width: 485px;*/
+max-width: 485px;
+min-width: 325px;
 `
-
-const Span = styled.span`
-margin: 5px;
+const Container = styled.div`
+display: flex;
+/* margin: 10px; */
+width: 1155px;
+overflow: hidden;
+margin: 0px auto;
+position: relative;
+height: 550px;
+`
+const ContainerPage = styled.div`
+display: flex;
+margin: 0;
+position: absolute; 
+left: 0;
+top: 0;
+transition: all ease 1s;
+`
+const Button = styled.button`
+    margin: 5px 2px;
+    background: #dbe3e5;
+    border: none;
+    color: #2e2b2b;
+    padding: 5px 10px;
+    border-radius: 5px;
+    font-size: 20px;
+    cursor: pointer;
+    width: 50px;
 cursor: pointer;
 `
+const Buttonleft = styled.button`
+margin: 5px 2px;
+background: #4e4a4f;
+border: none;
+color: white;
+padding: 5px 10px;
+border-radius: 5px;
+font-size: 20px;
+width: 50px;
+cursor: pointer;
+`
+
